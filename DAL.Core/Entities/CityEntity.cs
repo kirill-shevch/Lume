6cc4fe07c160247941330f134664b88615061f47
Kirill Shevchenko <kirillwowlol@yandex.ru>
2020-07-28 14:47:54 +0400

    Add new entity for cities

---
+++ DAL.Core/Entities/CityEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace DAL.Core.Entities
+{
+	public class CityEntity
+	{
+		public const string TableName = "City";
+		public long CityId { get; set; }
+		public string CityName { get; set; }
+	}
+}
