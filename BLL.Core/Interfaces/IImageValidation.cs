9c8d5b52837d526a33a1d2d994504b5d33f60cfe
Kirill Shevchenko <kirillwowlol@yandex.ru>
2020-08-10 19:36:30 +0400

    Remove useless methods from ImageController

--- BLL.Core/Interfaces/IImageValidation.cs
+++ BLL.Core/Interfaces/IImageValidation.cs
@@ -5,9 +5,6 @@ namespace BLL.Core.Interfaces
 {
 	public interface IImageValidation
 	{
-		(bool ValidationResult, string ValidationMessage) ValidateAddPersonImage(AddPersonImageModel model, Guid personUid);
-		(bool ValidationResult, string ValidationMessage) ValidateGetChatMessageImage(Guid uid);
-		(bool ValidationResult, string ValidationMessage) ValidateGetEventImage(Guid uid);
-		(bool ValidationResult, string ValidationMessage) ValidateGetPersonImage(Guid uid);
+		(bool ValidationResult, string ValidationMessage) ValidateGetImage(Guid uid);
 	}
 }
