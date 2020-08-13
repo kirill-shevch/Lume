5a54fb0a0e1865dcda353dd743666396fa64bd03
Kirill Shevchenko <kirillwowlol@yandex.ru>
2020-08-13 15:52:10 +0400

    Add dal for last read message

--- DAL.Core/Entities/PersonToChatEntity.cs
+++ DAL.Core/Entities/PersonToChatEntity.cs
@@ -6,6 +6,7 @@
 		public long FirstPersonId { get; set; }
 		public long SecondPersonId { get; set; }
 		public long ChatId { get; set; }
+		public long? LastReadChatMessageId { get; set; }
 		public ChatEntity Chat{ get; set; }
 		public PersonEntity FirstPerson { get; set; }
 		public PersonEntity SecondPerson { get; set; }
--- DAL.Core/Entities/PersonToEventEntity.cs
+++ DAL.Core/Entities/PersonToEventEntity.cs
@@ -6,6 +6,7 @@
 		public long PersonId { get; set; }
 		public long EventId { get; set; }
 		public long? ParticipantStatusId { get; set; }
+		public long? LastReadChatMessageId { get; set; }
 		public PersonEntity Person { get; set; }
 		public EventEntity Event { get; set; }
 		public ParticipantStatusEntity ParticipantStatus { get; set; }
