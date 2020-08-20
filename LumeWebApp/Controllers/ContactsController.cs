b5d29d11c7b4867b55596e083bb188bacb5d44a5
Kirill Shevchenko <kirillwowlol@yandex.ru>
2020-08-20 18:32:05 +0400

    Return person models during imporing person list from contacts

--- LumeWebApp/Controllers/ContactsController.cs
+++ LumeWebApp/Controllers/ContactsController.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BLL.Authorization.Interfaces;
-using Microsoft.AspNetCore.Http;
+﻿using BLL.Authorization.Interfaces;
+using BLL.Core.Interfaces;
+using BLL.Core.Models.Person;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace LumeWebApp.Controllers
 {
@@ -13,16 +12,26 @@ namespace LumeWebApp.Controllers
 	public class ContactsController : ControllerBase
 	{
 		private readonly IAuthorizationLogic _authorizationLogic;
-		public ContactsController(IAuthorizationLogic authorizationLogic)
+		private readonly IPersonLogic _personLogic;
+		public ContactsController(IAuthorizationLogic authorizationLogic,
+			IPersonLogic personLogic)
 		{
 			_authorizationLogic = authorizationLogic;
+			_personLogic = personLogic;
 		}
 		
 		[HttpPost]
 		[Route("get-person-list-by-contacts")]
-		public async Task<ActionResult<List<Guid>>> GetPersonListByContacts(List<string> phoneNumbers)
+		public async Task<ActionResult<List<PersonModel>>> GetPersonListByContacts(List<string> phoneNumbers)
 		{
-			return await _authorizationLogic.GetPersonListByContacts(phoneNumbers);
+			var personUids = await _authorizationLogic.GetPersonListByContacts(phoneNumbers);
+			var personModelList = new List<PersonModel>();
+			foreach (var uid in personUids)
+			{
+				var model = await _personLogic.GetPerson(uid);
+				personModelList.Add(model);
+			}
+			return personModelList;
 		}
 	}
 }
