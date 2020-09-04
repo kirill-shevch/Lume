6d48af3e3b0c8b7a57666f34b19a6bae0b754ae9
Kirill Shevchenko <kirillwowlol@yandex.ru>
2020-09-04 19:20:03 +0400

    took out the sms flag into azure configurations

--- Constants/ConfigurationKeys.cs
+++ Constants/ConfigurationKeys.cs
@@ -7,6 +7,7 @@
 		public const string SmsSendingApiKey = "SMSSENDING_APIKEY";
 		public const string SmsServiceUrl = "SmsServiceUrl";
 		public const string EnableSmsService = "EnableSmsService";
+		public const string AzureEnableSmsService = "AZURE_ENABLESMSSERVICE";
 		public const string FirebaseKey = "FIREBASE_KEY";
 		public const string AzureStorageConnectionString = "AZURESTORAGE_CONNECTIONSTRING";
 		public const string LocalAzureStorageConnectionString = "LocalAzureStorageConnectionString";
