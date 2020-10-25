4e98dd3a5de20f68a85cca60c959bd9b2bd45920
Kirill Shevchenko <kirillwowlol@yandex.ru>
2020-10-25 19:19:33 +0400

    Add new DAL layer for reports

---
+++ DAL.Core/Configurations/EventReportEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Core.Configurations
+{
+	public class EventReportEntityConfiguration : IEntityTypeConfiguration<EventReportEntity>
+	{
+		public void Configure(EntityTypeBuilder<EventReportEntity> builder)
+		{
+			builder.ToTable(EventReportEntity.TableName);
+			builder.HasKey(t => t.EventReportId);
+			builder.Property(t => t.EventReportUid);
+			builder.Property(t => t.Text);
+			builder.Property(t => t.CreationTime);
+			builder.Property(t => t.IsProcessed);
+			builder.Property(t => t.EventId);
+			builder.HasOne(t => t.Event).WithMany().HasForeignKey(x => x.EventId);
+		}
+	}
+}
---
+++ DAL.Core/Entities/PersonReportEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DAL.Core.Entities
+{
+	public class PersonReportEntity
+	{
+		public const string TableName = "PersonReport";
+		public long PersonReportId { get; set; }
+		public Guid PersonReportUid { get; set; }
+		public string Text { get; set; }
+		public DateTime CreationTime { get; set; }
+		public long PersonId { get; set; }
+		public PersonEntity Person { get; set; }
+		public bool IsProcessed { get; set; }
+	}
+}
